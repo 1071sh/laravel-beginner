39474cba8f515d9aed0d67b10b3d6101229433ab
nishida <k.nishida@vistage-llc.com>
2020-06-08 21:15:48 +0900

    chapter4

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -4,31 +4,35 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use App\Http\Requests\HelloRequest;
+use Validator;
 
 class HelloController extends Controller
 {
-    public function index(Request $request, Response $response)
+    public function index(Request $request)
     {
-        $html = <<<EOF
-<html>
-<head>
-<title>Hello/Index</title>
-<style>
-body {font-size:16pt; color:#999; }
-h1 { font-size:120pt; text-align:right; color:#fafafa;
-  margin:-50px 0px -120px 0px; }
-</style>
-</head>
-<body>
-  <h1>Hello</h1>
-  <h3>Request</h3>
-  <pre>{$request}</pre>
-  <h3>Response</h3>
-  <pre>{$response}</pre>
-</body>
-</html>
-EOF;
-        $response->setContent($html);
+        if ($request->hasCookie('msg')) {
+            $msg = 'Cookie: ' . $request->cookie('msg');
+        } else {
+            $msg = '※クッキーはありません。';
+        }
+        return view('hello.index', ['msg'=> $msg]);
+    }
+
+
+    public function post(Request $request)
+    {
+        $validate_rule = [
+            'msg' => 'required',
+        ];
+        $this->validate($request, $validate_rule);
+        $msg = $request->msg;
+        $response = response()->view(
+            'hello.index',
+            ['msg'=>'「' . $msg .
+            '」をクッキーに保存しました。']
+        );
+        $response->cookie('msg', $msg, 100);
         return $response;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\HelloMiddleware;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,8 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('hello', 'HelloController@index');
+Route::post('hello', 'HelloController@post');
